6a1772e7b7e01246ebef2150d9473e7c1fc124fb
LucaLeFich <pq48wkn@eduvaud.ch>
2025-01-20 22:31:30 +0100

    (FEAT) ajout des tours
    une tour pour chaque joueurs (sera ensuite destructible)

--- Projet jeu 320/Program.cs
+++ Projet jeu 320/Program.cs
@@ -10,20 +10,24 @@ namespace Projet_320_Stone_Sling
     {
         static void Main(string[] args)
         {
+            //Taille de la console
             Console.WindowHeight = 40;
             Console.WindowWidth = 150;
 
             // Création des joueurs
             Joueur j1 = new Joueur { Color = ConsoleColor.Blue }; //Joueur 1 en bleu
             Joueur j2 = new Joueur { Color = ConsoleColor.Red }; //Joueur 2 en rouge
+            Tours t1 = new Tours();
+            Tours t2 = new Tours();
 
             // Coordonnées pour affichage joueurs
-            j1.Afficher(10, 5);
-            j2.Afficher(30, 5);
+            j1.Afficher(10, 27);
+            j2.Afficher(108, 27);
+            t1.Afficher(20, 23);
+            t2.Afficher(98, 23);
+            
 
-            Console.ResetColor();
             Console.ReadLine();
         }
-
     }
 }
---
+++ Projet jeu 320/Tours.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_320_Stone_Sling
+{
+    internal class Tours
+    {
+        public string[] _tours {  get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public Tours() 
+        {
+            _tours = new string[]
+            {
+                @"####",
+                @"####",
+                @"####",
+                @"####",
+                @"####",
+                @"####",
+                @"####",
+                @"####"
+            };
+            Color = ConsoleColor.Yellow; //couleur par défaut
+        }
+
+        public void Afficher(int x, int y)
+        {
+            Console.ForegroundColor = Color;
+
+            for (int i = 0; i < _tours.Length; i++)
+            {
+                Console.SetCursorPosition(x, y + i);
+                Console.Write(_tours[i]);
+            }
+            Console.ResetColor(); // Réinitialise la couleur par défaut
+        }
+    }
+}
